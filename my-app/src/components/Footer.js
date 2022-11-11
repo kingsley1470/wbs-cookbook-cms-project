f189262825e3208092ea6ea03bb0736cb2ee8e9c
roxibi <106161599+roxibi@users.noreply.github.com>
2022-11-11 16:01:07 +0100

    styling footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -6,15 +6,18 @@ function Footer() {
   let { reviews } = Reviews;
 
   return (
+    <> <h2 className="reviewsH3">Reviews from around the globe...</h2>
     <div className="footerComponent">     
+   
       {reviews.map((r) => (
-        <div>
-          <h3>"{r.quote}"</h3>
+        <div className="footerReview">
+          <h3><i>"{r.quote}"</i></h3>
           <p>{r.name}</p>
           <p>{r.year}</p>
         </div>
       ))}
     </div>
+    </>
   );
 }
 
