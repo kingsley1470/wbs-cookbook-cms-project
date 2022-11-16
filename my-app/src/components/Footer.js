2d14e3c48044ebd741edc9f347b5699775922019
Nizami Suleymanov <nizami.suleymanoff@gmail.com>
2022-11-16 18:48:44 +0100

    adding images in footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -14,6 +14,7 @@ function Footer() {
           <h3><i>"{r.quote}"</i></h3>
           <p>{r.name}</p>
           <p>{r.year}</p>
+          <img className="footerImages" src={r.image}></img>
         </div>
       ))}
     </div>
